e97cc5f08d903c1ebaab670c02f244fbe55d5a4e
ericmullally <ericmullally@yahoo.com>
2022-02-07 15:57:35 -0600

    main page images added and layout set

---
+++ blogExapmle/static/JS/index.js
@@ -0,0 +1,7 @@
+var hamburger = document.getElementById("hamburger-div");
+
+hamburger.onclick = foo;
+
+function foo(){
+    console.log("clicked")
+}
--- blogExapmle/views.py
+++ blogExapmle/views.py
@@ -1,5 +1,36 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def home_page(request):
-    return render(request, "index.html")
+    blogContient = [
+        {
+            "img": "static/IMGS/daniel-jensen-NMk1Vggt2hg-unsplash.jpg",
+            "title": "title 1",
+            "paragraph": "Minim in minim ad duis proident qui cillum cillum laborum dolore.\
+                    Minim in minim ad duis proident qui cillum cillum laborum dolore.\
+                    Minim in minim ad duis proident qui cillum cillum laborum dolore.",
+            "url": "/title 1"
+        },
+        {
+            "img": "static/IMGS/m-wrona-pCgxm-HDMNs-unsplash.jpg",
+            "title": "title 2",
+            "paragraph": "Minim in minim ad duis proident qui cillum cillum laborum dolore.\
+                    Minim in minim ad duis proident qui cillum cillum laborum dolore.\
+                    Minim in minim ad duis proident qui cillum cillum laborum dolore.",
+            "url": "/title 1"
+        },
+
+        {
+            "img": "static/IMGS/gwendal-cottin-j85t8FTaCcE-unsplash.jpg",
+            "title": "title 3",
+            "paragraph": "Minim in minim ad duis proident qui cillum cillum laborum dolore.\
+                    Minim in minim ad duis proident qui cillum cillum laborum dolore.\
+                    Minim in minim ad duis proident qui cillum cillum laborum dolore.",
+            "url": "/title 1"
+
+        }
+    ]
+    content = {"blogContient": blogContient}
+
+    return render(request, "index.html", context=content)
