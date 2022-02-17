7629dddb47da870b6f8241e8c49d10bb8573298c
ericmullally <ericmullally@yahoo.com>
2022-02-17 12:53:35 -0600

    begin db work

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    Id = models.IntegerField(primary_key=True, unique=True, auto_created=True)
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=50)
+    body = models.CharField(max_length=5000)
+    img = models.CharField(max_length=100)
